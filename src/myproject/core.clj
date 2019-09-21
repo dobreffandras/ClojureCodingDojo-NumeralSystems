b611984d6eedc6e96e97732322dade2145618938
András Dobreff <dobreff.andras@sonrisa.hu>
2019-09-21 09:57:02 +0200

    Add proper validation checks only for arabic

--- src/myproject/core.clj
+++ src/myproject/core.clj
@@ -6,13 +6,18 @@
 	(some true? [(= lang "Arabic") (= lang "Roman") (= lang "Urnfield")]))
 
 (defn is-arabic [number-string] 
-	(every number? (seq number-string)))
+	(every? #(< -1 %) (map #(Character/digit % 10) (seq number-string))))
 
-(defn is-roman [number-string] 
-	(every #(contains? ["I", "V", "X", "L", "C", "D", "M"]) (seq number-string)))
+;;(defn is-roman [number-string] 
+;;	(every? #(contains? ["I", "V", "X", "L", "C", "D", "M"] %) (seq number-string)))
+
+;;(defn is-urnfield [number-string] 
+;;	(every? #(contains? ["\", "/"] %) (seq number-string)))
 
 (defn is-valid-number [number] 
-	(some true? [(is-arabic number) (is-roman number) (is-urnfield number)])
+	;;(some true? [(is-arabic number) (is-roman number) (is-urnfield number)])
+	(some true? [(is-arabic number)])
+	)
 
 (defn valid-input [x] 
 	(and 
@@ -30,7 +35,8 @@
 	"Converts between numeral systems or throws exception if input is not valid"
 	[arguments]
 	(if (valid-input arguments)
-		(let [[from to number] arguments](convert-number from to number))
+		(let [[from to number] arguments]
+			(convert-number from to number))
 		(throw (ParseException. "Wrong input" 0))))
 
 (defn -main [ & args ]
